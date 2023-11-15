898d3f495b58c6431b8d18535fc08ab2645a2ca8
Claire Ciosi <claire.lewagon@gmail.com>
2023-11-15 14:25:25 +0000

    Calcul Margin Percent

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,6 +1,8 @@
-SELECT *,
+WITH purchase_cost_table AS
+
+(SELECT
+*, 
 quantity * purchase_price AS purchase_cost, 
-ROUND(revenue - (quantity * purchase_price),2) AS margin,
 
 FROM 
 {{ref("stg_raw__sales")}}
@@ -8,4 +10,12 @@ FROM
 LEFT JOIN
 {{ref("stg_raw__product")}}
 USING (products_id)
+)
+
+SELECT
+*,
+ROUND(revenue - (quantity * purchase_price),2) AS margin,
+{{margin_percent(revenue,purchase_cost)}} AS margin_percent,
+
+FROM purchase_cost_table
 
